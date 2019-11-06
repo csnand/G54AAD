1db5681536c31cd85d76b06902887a45188f4f8f
csnand <askmryang@outlook.com>
2019-11-06 20:07:46 +0000

    cw2 init

--- shortestPath/src/ShortPaths.java
+++ shortestPath/src/ShortPaths.java
@@ -1,6 +1,20 @@
+import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Scanner;
+
 public class ShortPaths {
 
     public static void main(String[] args) {
-        System.out.println("hello, world");
+
+        if (args.length == 0) {
+            System.out.println("Please input  path to a test file");
+            return;
+        }
+
+        String fileName = args[1];
+        Path path = Paths.get(fileName);
+        Scanner sc = new Scanner(System.in);
+
     }
 }
